851e75406ab6d7ceb7e98045f6c1319d5e435a92
roofclimb <s10243726@connect.np.edu.sg>
2023-05-03 22:38:02 +0800

    add user

--- app/src/main/java/sg/edu/np/mad/test/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/test/MainActivity.java
@@ -6,25 +6,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView message;
     User user;
+
 
     final String TAG="Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //user=new User("John","hello", 1234,true);
+        user=new User("John","hello", 1234,true);
     }
     @Override
     protected void onStart(){
         super.onStart();
         Log.v(TAG,"on start!!");
-        message=(TextView)findViewById(R.id.button1);
+        //message=findViewById(R.id.button1);
         Button buttonA = findViewById(R.id.button1);
         buttonA.setOnClickListener(new View.OnClickListener() {
 
@@ -32,17 +31,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.v(TAG,"Button A clicked");
                 if(user.followed){
+                    buttonA.setText("Follow");
                     user.followed=false;
-                    //message.setText("Followed");
                 }else{
+                    buttonA.setText("Unfollow");
                     user.followed=true;
-                    //message.setText("Unfollow");
-                }
-                if(user.followed)
-                {
-                    message.setText("Follow");
-                }else{
-                    message.setText("Unfollow");
                 }
             }
 
