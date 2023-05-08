dbc6e889bd706fe0406af76f985d123e3f83dd7f
roofclimb <s10243726@connect.np.edu.sg>
2023-05-08 20:22:47 +0800

    extra changes

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -0,0 +1,50 @@
+package sg.edu.np.mad.madpractical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    User user;
+
+    final String TAG="Main Activity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        user=new User("John","hello", 1234,true);
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.v(TAG,"on start!!");
+        Button buttonA = findViewById(R.id.button1);
+        buttonA.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG,"Button A clicked");
+                if(user.followed){
+                    user.followed=false;
+                    //message.setText("Followed");
+                }else{
+                    user.followed=true;
+                    //message.setText("Unfollow");
+                }
+                if(user.followed)
+                {
+                    buttonA.setText("Follow");
+                }else{
+                    buttonA.setText("Unfollow");
+                }
+            }
+
+        });
+
+    }
+}
